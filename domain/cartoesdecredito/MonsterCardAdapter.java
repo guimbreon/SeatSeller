0197e6d74f4b6023516c31c36464fd2f584abff3
guimbreon <gui.acs26@gmail.com>
2025-05-23 15:48:04 +0100

    domain\cartoesdecredito\
    MonsterCardAdapter -> Complete
    PortgueseExpress -> Working on it.

--- domain/cartoesdecredito/MonsterCardAdapter.java
+++ domain/cartoesdecredito/MonsterCardAdapter.java
@@ -1,5 +1,40 @@
 package domain.cartoesdecredito;
 
-public class MonsterCardAdapter {
+import monstercard.Card;
+import monstercard.MonsterCardAPI;
 
+public class MonsterCardAdapter implements ISistemaDeCartoesDeCreditoAdapter {
+
+	private MonsterCardAPI api = new MonsterCardAPI();
+
+	@Override
+	public boolean validar(String num, int ccv, int mes, int ano) {
+		String mesStr = String.format("%02d", mes);
+		String anoStr = String.format("%02d", ano); // 4digitos
+		Card card = new Card(num, String.valueOf(ccv), mesStr, anoStr);
+		
+		return api.isValid(card);
+	}
+
+	@Override
+	public boolean cativar(String num, int ccv, int mes, int ano, double qt) {
+		String mesStr = String.format("%02d", mes);
+		String anoStr = String.format("%02d", ano); // 4digitos
+		Card card = new Card(num, String.valueOf(ccv), mesStr, anoStr);
+		if (!api.isValid(card)) return false;
+
+		api.block(card, qt);
+		return true;
+	}
+
+	@Override
+	public boolean retirar(String num, int ccv, int mes, int ano, double qt) {
+		String mesStr = String.format("%02d", mes);
+		String anoStr = String.format("%02d", ano); // 4digitos
+		Card card = new Card(num, String.valueOf(ccv), mesStr, anoStr);
+		if (!api.isValid(card)) return false;
+
+		api.charge(card, qt);
+		return true;
+	}
 }
--- domain/cartoesdecredito/PortugueseExpressAdapter.java
+++ domain/cartoesdecredito/PortugueseExpressAdapter.java
@@ -1,5 +1,30 @@
 package domain.cartoesdecredito;
 
-public class PortugueseExpressAdapter {
+import portugueseexpress.PortugueseExpress;
+
+public class PortugueseExpressAdapter implements ISistemaDeCartoesDeCreditoAdapter{
+
+	@Override
+	public boolean validar(String num, int ccv, int mes, int ano) {
+		PortugueseExpress api = new PortugueseExpress();
+		api.setNumber("1234123412341234");
+		api.setCcv(123);
+		api.setMonth(5);
+		api.setYear(2018);
+		return api.validate();
+		
+	}
+
+	@Override
+	public boolean cativar(String num, int ccv, int mes, int ano, double qt) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retirar(String num, int ccv, int mes, int ano, double qt) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
