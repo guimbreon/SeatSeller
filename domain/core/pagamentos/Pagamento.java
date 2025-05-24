159281d04abda63bd6f4dc6fa213d90112baec95
guimbreon <gui.acs26@gmail.com>
2025-05-24 20:54:37 +0100

    domain.core.pagamentos

--- domain/core/pagamentos/Pagamento.java
+++ domain/core/pagamentos/Pagamento.java
@@ -1,5 +1,23 @@
 package domain.core.pagamentos;
 
 public class Pagamento {
-	//for utilizador
+
+    private String tipo;
+    private double valor;
+
+    // Construtor
+    public Pagamento(String tipo, double valor) {
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    
+    public String getTipo() {
+        return tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
 }
