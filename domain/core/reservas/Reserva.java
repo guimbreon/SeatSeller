a6a4594a9826e166dd9a6be48ba237765f7489c3
faresk <durt1005@gmail.com>
2025-05-24 17:25:10 +0100

    reserva alpha
    Reserva pre release edition

--- domain/core/reservas/Reserva.java
+++ domain/core/reservas/Reserva.java
@@ -1,5 +1,34 @@
 package domain.core.reservas;
 
-public class Reserva {
+import domain.core.pagamentos.Pagamento; 
+import java.util.List;
+import java.util.ArrayList;
+import java.time.LocalDate;
 
+public class Reserva {
+    private int id;
+    private LocalDateTime dataReserva;
+    private List<LinhaReserva> linhasReserva;
+
+    public Reserva(){
+        this.id = id;
+        this.dataReserva = dataReserva;
+        this.linhasReserva = new ArrayList<>();
+    }
+
+    public LocalDate getdate() {
+        return dataReserva.toLocalDate();
+    }
+
+    public LocalTime getTime() {
+        return dataReserva.toLocalTime();
+    }
+
+    public double getSubtotal() {
+        double subtotal = 0.0;
+        for (LinhaReserva linha : linhasReserva) {
+            subtotal += linha.getPreco();
+        }
+        return subtotal;
+    }
 }
