9306da57dccd3f08411f10b2b2d74e709b1a99b2
vitoria <vitoriateixeiracorreia04@gmail.com>
2025-05-25 16:24:10 +0100

    domain.core.lugares.alocacao.Aleatorio

--- domain/core/lugares/alocacao/LugarAleatorioStrategy.java
+++ domain/core/lugares/alocacao/LugarAleatorioStrategy.java
@@ -1,5 +1,27 @@
 package domain.core.lugares.alocacao;
 
-public class LugarAleatorioStrategy {
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import domain.core.lugares.Grelha;
+import domain.core.lugares.Lugar;
+import domain.core.lugares.TipoDeLugar;
+
+public class LugarAleatorioStrategy implements IEncontrarLugarStrategy {
+
+	public Optional<Lugar> getLugar(Grelha grelha, TipoDeLugar tp, String data, String hora) {
+		List<Lugar> candidatos = grelha.getLugares().stream()
+	            .filter(l -> l.getDesignacaoTipo() != null)
+	            .filter(l -> l.getDesignacaoTipo().equals(tp.getDesig()))
+	            .filter(l -> l.disponivel(data, hora))
+	            .collect(Collectors.toList());
+
+	        Collections.shuffle(candidatos);
+
+	        return candidatos.stream().findFirst();
+	}
+	
+	
 }
