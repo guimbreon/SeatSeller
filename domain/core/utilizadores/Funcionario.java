36462379dc8f133d3fe86f1af26c0c45fd0222e0
faresk <durt1005@gmail.com>
2025-05-25 15:50:05 +0100

    funcionario

--- domain/core/utilizadores/Funcionario.java
+++ domain/core/utilizadores/Funcionario.java
@@ -1,5 +1,38 @@
 package domain.core.utilizadores;
 
-public class Funcionario extends Utilizador implements PropertyChangeListener {
-    
+import domain.core.lugares.Grelha;
+import domain.api.INotificacaoReceiver;
+import java.beans.PropertyChangeListener;
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+public class Funcionario extends Utilizador 	{
+    private final Set<INotificacaoReceiver> receivers;
+	
+	
+	public Funcionario(String username, String password) {
+		super(username, password);
+		this.receivers = new HashSet<>();
+	}
+	
+	public void associarGrelha(Grelha g, INotificacaoReceiver notR) {
+		g.addPropertyChangeListener(this);
+		adicionaNotificationReceiver(notR);
+	}
+
+	public void desassociarGrelha(Grelha g, INotificacaoReceiver notR) {
+		g.removePropertyChangeListener(this);
+		removeNotificationReceiver(notR);
+	}
+	
+	public void adicionaNotificationReceiver(INotificacaoReceiver receiver) {
+		receivers.add(receiver);
+	}
+	
+	public void removeNotificationReceiver(INotificacaoReceiver receiver) {
+	    receivers.remove(receiver);
+	}
+	
 }
