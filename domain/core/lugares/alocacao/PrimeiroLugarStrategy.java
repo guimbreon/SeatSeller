847ddf46d2df1b7a480b4e885ab5b7063f938afe
vitoria <vitoriateixeiracorreia04@gmail.com>
2025-05-25 15:39:54 +0100

    domain.lugares.alocacao.PrimeiroLugar

--- domain/core/lugares/alocacao/PrimeiroLugarStrategy.java
+++ domain/core/lugares/alocacao/PrimeiroLugarStrategy.java
@@ -1,5 +1,18 @@
 package domain.core.lugares.alocacao;
 
+import java.util.Optional;
+
+import domain.core.lugares.Grelha;
+import domain.core.lugares.Lugar;
+import domain.core.lugares.TipoDeLugar;
+
 public class PrimeiroLugarStrategy {
 
+	public Optional<Lugar> getLugar(Grelha grelha, TipoDeLugar tp, String data, String hora) {
+		return grelha.getLugares().stream()
+	            .filter(lugar -> lugar.getDesignacaoTipo() != null)
+	            .filter(lugar -> lugar.getDesignacaoTipo().equals(tp.getDesig()))
+	            .filter(lugar -> lugar.disponivel(data, hora))
+	            .findFirst();
+	}	
 }
